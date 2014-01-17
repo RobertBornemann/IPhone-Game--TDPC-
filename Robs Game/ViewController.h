1f1b44dc2879e0cee6232e9ca76d1ff35b334878
Robert Bornemann <bornemann.robert@yahoo.de>
2014-01-17 18:48:56 +0100

    more code

--- Robs Game/ViewController.h
+++ Robs Game/ViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 int Y;
 BOOL Start;
+int RandomPosition;
 
 @interface ViewController : UIViewController
 
@@ -21,6 +22,23 @@ BOOL Start;
     IBOutlet UIImageView *Heli;
     
     NSTimer *timer;
+    
+    IBOutlet UIImageView * Obstacle;
+    IBOutlet UIImageView * Obstacle2;
+    IBOutlet UIImageView * Bottom1;
+    IBOutlet UIImageView * Bottom2;
+    IBOutlet UIImageView * Bottom3;
+    IBOutlet UIImageView * Bottom4;
+    IBOutlet UIImageView * Bottom5;
+    IBOutlet UIImageView * Bottom6;
+    IBOutlet UIImageView * Bottom7;
+    IBOutlet UIImageView * Top1;
+    IBOutlet UIImageView * Top2;
+    IBOutlet UIImageView * Top3;
+    IBOutlet UIImageView * Top4;
+    IBOutlet UIImageView * Top5;
+    IBOutlet UIImageView * Top6;
+    IBOutlet UIImageView * Top7;
 
 }
 -(void)HeliMove;
